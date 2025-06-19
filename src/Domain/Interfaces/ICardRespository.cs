0bccb303a42876023b0005a3dcfffdf2e0419434
Shawn Kuo <were1122was@gmail.com>
2025-06-19 23:51:08 +0800

    add card interface

---
+++ src/Domain/Interfaces/ICardRespository.cs
@@ -0,0 +1,10 @@
+﻿using MetroPass.Domain.Entities;
+
+namespace MetroPass.Domain.Interfaces;
+
+public interface ICardRespository
+{
+    Task<Card?> GetCardByCardIdAsync(int cardId);
+
+    Task SaveAsync(Card card);
+}
